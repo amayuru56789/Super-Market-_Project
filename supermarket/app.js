de5007ac790bb556d2699a7a6bb4e68077573e67
amayuru56789 <indeewara5678@gmail.com>
2022-09-07 15:28:18 +0530

    server port open

--- supermarket/app.js
+++ supermarket/app.js
@@ -6,23 +6,23 @@ const port = 4000;
 // if we use json obejct inside our app.js
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    console.log('Get Request has come')
-    res.send('Hello KITT!')
-})
+// app.get('/', (req, res) => {
+//     console.log('Get Request has come')
+//     res.send('Hello KITT!')
+// })
 
-app.get('/customer', (req, res) => {
-    console.log(req.body)
-    res.send('Hello Customer')
-})
+// app.get('/customer', (req, res) => {
+//     console.log(req.body)
+//     res.send('Hello Customer')
+// })
 
-app.get('/customer/:id', (req, res) => {
-    console.log(req.params);
-})
+// app.get('/customer/:id', (req, res) => {
+//     console.log(req.params);
+// })
 
-app.get('/customer', (req, res) => {
-    console.log(req.params.id);
-})
+// app.get('/customer', (req, res) => {
+//     console.log(req.params.id);
+// })
 
 app.listen(port, (req, res) => {
     console.log(`Express app listening on port ${port}`);
