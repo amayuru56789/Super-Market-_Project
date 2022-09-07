7b9ea2d191694d5bbe89d26ac3d663ed9ff55e4a
amayuru56789 <indeewara5678@gmail.com>
2022-09-07 18:54:55 +0530

    item middleware done

--- supermarket/app.js
+++ supermarket/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const customer = require('./routes/customer')
+const item = require('./routes/item')
 const app = express();
 
 const port = 4000;
@@ -7,24 +8,30 @@ const port = 4000;
 // if we use json obejct inside our app.js
 app.use(express.json());
 app.use('/customer', customer)
+app.use('/item',  item)
 
 app.get('/', (req, res) => {
     console.log('get request comming!');
     res.send('get req came for / route')
 })
 
-app.post('/', (req, res) => {
-    res.send('<h2>post request came for / route</h2>')
-})
+// app.post('/', (req, res) => {
+//     res.send('<h2>post request came for / route</h2>')
+// })
 
-app.get('/customer', (req, res) => {
-    console.log('customer get come');
-    res.send('<h1>Customer get req came</h1>')
-})
+// app.get('/customer', (req, res) => {
+//     console.log('customer get come');
+//     res.send('<h1>Customer get req came</h1>')
+// })
 
-app.post('/customer', (req, res) => {
-    res.send('customer post req came')
-})
+// app.post('/customer', (req, res) => {
+//     console.log(req.body.id);
+//     res.send('customer post req came')
+// })
+
+// app.delete('/customer', (req, res) => {
+//     res.send('delete req came for customer')
+// })
 
 // app.get('/', (req, res) => {
 //     console.log('Get Request has come')
