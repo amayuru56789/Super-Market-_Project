a47216053192cd5ec6acb77e8e384d77c5ac0350
amayuru56789 <indeewara5678@gmail.com>
2022-09-07 14:57:09 +0530

    customer get request

--- supermarket/app.js
+++ supermarket/app.js
@@ -3,13 +3,25 @@ const app = express();
 
 const port = 4000;
 
+// if we use json obejct inside our app.js
+app.use(express.json());
+
 app.get('/', (req, res) => {
     console.log('Get Request has come')
     res.send('Hello KITT!')
 })
 
 app.get('/customer', (req, res) => {
+    console.log(req.body)
     res.send('Hello Customer')
+})
+
+app.get('/customer/:id', (req, res) => {
+    console.log(req.params);
+})
+
+app.get('/customer', (req, res) => {
+    console.log(req.params.id);
 })
 
 app.listen(port, (req, res) => {
